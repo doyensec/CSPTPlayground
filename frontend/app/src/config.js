511e595575ce586caf78874d581c760798617cba
Maxence Schmitt <maxence@doyensec.com>
2025-01-05 16:46:43 +0100

    Fix the harcoded localhost in the Frontend

--- frontend/app/src/config.js
+++ frontend/app/src/config.js
@@ -1,5 +1,9 @@
+
+const BACKEND_HOST = process.env.REACT_APP_BACKEND_HOST ? process.env.REACT_APP_BACKEND_HOST : "localhost"
+const BACKEND_PORT = process.env.REACT_APP_BACKEND_PORT ? process.env.REACT_APP_BACKEND_PORT : "8000"
+
 const config = {
-  apiBaseUrl: 'http://localhost:8000'
+  apiBaseUrl: `http://${BACKEND_HOST}:${BACKEND_PORT}`
 };
 
 export default config;
